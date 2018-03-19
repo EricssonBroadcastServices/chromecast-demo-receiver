631bc1bbfeafd7a39263f25f05e0b2efa4a6776c
João Coelho <admin.henyah@gmail.com>
2018-03-19 13:04:44 +0100

    Making everything dependant on receiver dev by default. Release builds shall run release_version.sh before build to switch to release chromecast package.

---
+++ release_version.sh
@@ -0,0 +1,4 @@
+sed -i -e 's/emp-chromecast-receiver-2-dev/emp-chromecast-receiver-2/g' ./package.json
+sed -i -e 's/emp-chromecast-receiver-2-dev/emp-chromecast-receiver-2/g' ./app/js/emp-receiver-app.js
+sed -i -e 's/emp-chromecast-receiver-2-dev/emp-chromecast-receiver-2/g' ./build/grunt.js
+
