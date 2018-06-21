32be128acfd96c59b501b467f1b02db120bb37fe
empjenk <empjenk@ericsson.com>
2018-06-21 14:13:37 +0000

    Updated Demo CC

--- app/js/emp-shaka.js
+++ app/js/emp-shaka.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * EMP-Player 2.0.86-114 
+ * EMP-Player 2.0.86-115 
  * Copyright Ericsson, Inc. <https://www.ericsson.com/>
  */
 
@@ -4231,7 +4231,7 @@ EmpShaka.prototype['featuresNativeTextTracks'] = false;
 
 Tech.withSourceHandlers(EmpShaka);
 
-EmpShaka.VERSION = '2.0.86-114';
+EmpShaka.VERSION = '2.0.86-115';
 
 // Unset source handlers set by Html5 super class.
 // We do not intent to support any sources other then sources allowed by nativeSourceHandler
