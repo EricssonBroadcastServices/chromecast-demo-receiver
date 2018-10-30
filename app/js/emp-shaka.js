72cde043d8c63dde760e05b203af66ac0a16758c
empjenk <empjenk@ericsson.com>
2018-10-30 21:18:17 +0000

    Updated Demo CC

--- app/js/emp-shaka.js
+++ app/js/emp-shaka.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * EMP-Player 2.0.94-206 
+ * EMP-Player 2.0.94-207 
  * Copyright Ericsson, Inc. <https://www.ericsson.com/>
  */
 
@@ -3148,7 +3148,7 @@ var DownloadService = function (_Plugin) {
   return DownloadService;
 }(Plugin);
 
-DownloadService.VERSION = '2.0.94-206';
+DownloadService.VERSION = '2.0.94-207';
 
 if (videojs.getPlugin('DownloadService')) {
   videojs.log.warn('A plugin named "DownloadService" already exists.');
@@ -5388,7 +5388,7 @@ EmpShaka.prototype['featuresNativeTextTracks'] = false;
 
 Tech.withSourceHandlers(EmpShaka);
 
-EmpShaka.VERSION = '2.0.94-206';
+EmpShaka.VERSION = '2.0.94-207';
 
 // Unset source handlers set by Html5 super class.
 // We do not intent to support any sources other then sources allowed by nativeSourceHandler
