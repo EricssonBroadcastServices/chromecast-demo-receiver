f8018c8982db0ac8b58728e17bfabaa730156bac
empjenk <empjenk@ericsson.com>
2019-01-14 15:16:00 +0000

    Updated Demo CC

--- app/js/emp-player.js
+++ app/js/emp-player.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * EMP-Player 2.1.98-268 
+ * EMP-Player 2.1.98-274 
  * Copyright Ericsson, Inc. <https://www.ericsson.com/>
  */
 
@@ -7756,7 +7756,7 @@ var Player = function (_VjsPlayer) {
   createClass(Player, [{
     key: 'version',
     get: function get$$1() {
-      return '2.1.98-268';
+      return '2.1.98-274';
     }
 
     /**
@@ -8898,7 +8898,7 @@ var AnalyticsPlugin = function (_Plugin) {
   return AnalyticsPlugin;
 }(Plugin);
 
-AnalyticsPlugin.VERSION = '2.1.98-268';
+AnalyticsPlugin.VERSION = '2.1.98-274';
 
 if (videojs$1.getPlugin('analytics')) {
   videojs$1.log.warn('A plugin named "analytics" already exists.');
@@ -11843,7 +11843,7 @@ var ProgramService = function (_Plugin) {
   return ProgramService;
 }(Plugin$1);
 
-ProgramService.VERSION = '2.1.98-268';
+ProgramService.VERSION = '2.1.98-274';
 
 if (videojs.getPlugin('programService')) {
   videojs.log.warn('A plugin named "programService" already exists.');
@@ -12019,7 +12019,7 @@ var EntitlementExpirationService = function (_Plugin) {
   return EntitlementExpirationService;
 }(Plugin$2);
 
-EntitlementExpirationService.VERSION = '2.1.98-268';
+EntitlementExpirationService.VERSION = '2.1.98-274';
 
 if (videojs.getPlugin('entitlementExpirationService')) {
   videojs.log.warn('A plugin named "entitlementExpirationService" already exists.');
@@ -12502,7 +12502,7 @@ EntitlementMiddleware$1.registerEntitlementEngine = EntitlementEngine.registerEn
 
 EntitlementMiddleware$1.isEntitlementEngine = EntitlementEngine.isEntitlementEngine;
 
-EntitlementMiddleware$1.VERSION = '2.1.98-268';
+EntitlementMiddleware$1.VERSION = '2.1.98-274';
 
 if (videojs$1.EntitlementMiddleware) {
   videojs$1.log.warn('EntitlementMiddleware already exists.');
@@ -12632,7 +12632,7 @@ empPlayer.extend = videojs$1.extend;
  */
 empPlayer.Events = empPlayerEvents;
 
-empPlayer.VERSION = '2.1.98-268';
+empPlayer.VERSION = '2.1.98-274';
 
 /*
  * Universal Module Definition (UMD)
