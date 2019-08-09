bb60594bfc9ede359cfd540e7d719ac0c29f2e45
empjenk <empjenk@ericsson.com>
2019-08-09 15:55:11 +0000

    Updated Demo CC

--- app/js/emp-shaka.js
+++ app/js/emp-shaka.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * EMP-Player 2.1.110-446 
+ * EMP-Player 2.1.110-447 
  * Copyright Ericsson, Inc. <https://www.ericsson.com/>
  */
 
@@ -4631,7 +4631,7 @@
     return DownloadService;
   }(Plugin);
 
-  DownloadService.VERSION = '2.1.110-446';
+  DownloadService.VERSION = '2.1.110-447';
 
   if (videojs.getPlugin('DownloadService')) {
     videojs.log.warn('A plugin named "DownloadService" already exists.');
@@ -6243,7 +6243,7 @@
         // (this.hasStarted_ && this.el_.networkState > this.el_.HAVE_METADATA)) {
         // this.trigger('play'); not needed
         // return this.el_.play();
-        if (this.el() && this.hasMetadata_ && this.el_.networkState > this.el_.HAVE_METADATA) {
+        if (this.el() && this.hasMetadata_ && this.el_.networkState >= this.el_.HAVE_METADATA) {
           return _Html.prototype.play.call(this);
         }
       } else {
@@ -6462,7 +6462,7 @@
 
   EmpShaka.prototype.featuresNativeTextTracks = false;
   Tech$1.withSourceHandlers(EmpShaka);
-  EmpShaka.VERSION = '2.1.110-446'; // Unset source handlers set by Html5 super class.
+  EmpShaka.VERSION = '2.1.110-447'; // Unset source handlers set by Html5 super class.
   // We do not intent to support any sources other then sources allowed by nativeSourceHandler
 
   EmpShaka.sourceHandlers = [];
